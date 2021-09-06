b5b6a5568c44a6c54db28c81498191eae3d46fcc
Likhith JSC <52529829+likhith68@users.noreply.github.com>
2021-09-06 12:32:12 +0530

    Uploading code :)

---
+++ main.py
@@ -0,0 +1,21 @@
+from tkinter import *
+import random
+r=Tk()
+r.title("ROLL DICE")
+r.geometry("800x600")
+
+label=Label(r,font=('comics',250,'bold'),text='')
+
+#function defintion
+def RollDice():
+    roll=['\u2680','\u2681','\u2682','\u2683','\u2684','\u2685']
+
+    label.configure(text=random.choice(roll))
+    label.pack()
+
+#button creation
+button=Button(r,font=('comics',30,'bold'),text="Roll Dice",command=RollDice)
+button.pack()
+
+
+r.mainloop()
