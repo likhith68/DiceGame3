24162b7cd10ceec3ac7e65a4675cdececb3ba017
Likhith JSC <52529829+likhith68@users.noreply.github.com>
2021-09-06 12:33:33 +0530

    comments for label

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ r=Tk()
 r.title("ROLL DICE")
 r.geometry("800x600")
 
+#label defintion
 label=Label(r,font=('comics',250,'bold'),text='')
 
 #function defintion
 def RollDice():
     roll=['\u2680','\u2681','\u2682','\u2683','\u2684','\u2685']
 
-    label.configure(text=random.choice(roll))
+    label.configure(text=random.choice(roll))  #label configuration....configuring the label created in line 8
     label.pack()
 
 #button creation
