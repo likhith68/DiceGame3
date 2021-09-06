d5ae022eb640f88abbfa1d54112bb0f78cc82788
Likhith JSC <52529829+likhith68@users.noreply.github.com>
2021-09-06 12:34:44 +0530

    updating the label comments
    label pack comments

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def RollDice():
     roll=['\u2680','\u2681','\u2682','\u2683','\u2684','\u2685']
 
     label.configure(text=random.choice(roll))  #label configuration....configuring the label created in line 8
-    label.pack()
+    label.pack()   #Packing the label created in line 8 only after the configuration in line 14 is done
 
 #button creation
 button=Button(r,font=('comics',30,'bold'),text="Roll Dice",command=RollDice)
